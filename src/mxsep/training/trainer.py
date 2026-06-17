a1dafda6ada2cf4989aaa4e670c7dce3afb430a9
joris.vaneyghen <joris.vaneyghen@gmail.com>
2026-06-17 17:12:01 +0200

    fix

--- src/mxsep/training/trainer.py
+++ src/mxsep/training/trainer.py
@@ -364,7 +364,7 @@ class Trainer:
             print(f"Checkpoint {filename} not found, starting from scratch.")
             return
 
-        checkpoint = torch.load(checkpoint_path, map_location=self.device)
+        checkpoint = torch.load(checkpoint_path, map_location=self.device, weights_only=False)
         self.model.load_state_dict(checkpoint['model_state_dict'])
         self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         # if self.scheduler and checkpoint['scheduler_state_dict']:
